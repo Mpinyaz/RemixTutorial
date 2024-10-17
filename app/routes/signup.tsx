c9a1c1e2e2809237b23672a1c204ce1b22b85a37
Mpinyaz <ebmpinyuri@gmail.com>
2024-10-17 18:33:51 +0200

    applyied validation for signinup page using zod

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,44 +1,112 @@
-import { Link, useNavigate, useOutletContext } from "@remix-run/react";
+import {
+  Link,
+  useNavigate,
+  useOutletContext,
+  redirect,
+} from "@remix-run/react";
 import { toast } from "react-hot-toast";
-import React, { useRef } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { OutletContext } from "~/types";
+import { z } from "zod";
+import { type LoaderFunctionArgs } from "@remix-run/node";
+import { isUserLoggedIn } from "~/utils/auth.supabase.server";
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  if (await isUserLoggedIn(request)) {
+    return redirect("/");
+  }
+  return null;
+};
 export default function SignUp() {
   const navigate = useNavigate();
   const { supabase } = useOutletContext<OutletContext>();
-  const inputForm = useRef<HTMLFormElement>(null);
+  const SignUpSchema = z.object({
+    fullname: z.string().min(1, "Full name is required"),
+    password: z.string().min(6, "Password must have more than 6 characters"),
+    email: z.string().trim().email("Invalid email address"),
+  });
 
-  const handleLogin = async (event: React.FormEvent) => {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    if (!inputForm.current) return;
-
-    const formData = new FormData(inputForm.current);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const fullName = formData.get("fullname") as string;
-
-    // Supabase sign up logic
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: "http://localhost:5173/auth/credentials",
-        data: {
-          fullname: fullName,
-        },
-      },
-    });
+  type SignUp = z.infer<typeof SignUpSchema>;
+  type FormErrors = Partial<Record<keyof SignUp, string[]>>;
+  const [formData, setFormData] = useState<SignUp>({
+    fullname: "",
+    password: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [isEmailUnique, setIsEmailUnique] = useState(true);
+  const validateForm = (data: SignUp, field?: keyof FormData): FormErrors => {
+    try {
+      SignUpSchema.parse(data);
+      return field ? { [field]: [] } : {};
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const newErrors = error.flatten().fieldErrors;
+        return field ? { [field]: newErrors[field] || [] } : newErrors;
+      }
+      return {};
+    }
+  };
+  const checkEmailExists = async (email: string) => {
+    const { data, error } = await supabase
+      .from("users")
+      .select("email")
+      .eq("email", email);
 
     if (error) {
-      toast.error("Error signing up: " + error.message);
+      console.error("Error checking email: ", error);
+      return false;
+    }
+
+    return data.length > 0;
+  };
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newErrors = validateForm(formData);
+    setErrors(newErrors);
+    const emailExists = await checkEmailExists(formData.email);
+    setIsEmailUnique(!emailExists);
+
+    if (isEmailUnique) {
+      toast.error("Email already in use!");
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: ["Email already in use!"],
+      }));
       return;
     }
 
-    if (data) {
-      toast.success("A verification link has been sent to your email");
-      navigate("/"); // Navigate to homepage or a success page
+    if (Object.keys(newErrors).length === 0) {
+      const { data, error } = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password,
+        options: {
+          emailRedirectTo: "http://localhost:5173/auth/credentials",
+          data: {
+            fullname: formData.fullname,
+          },
+        },
+      });
+
+      if (error) {
+        toast.error("Error signing up: " + error.message);
+        return;
+      }
+
+      if (data) {
+        toast.success("A verification link has been sent to your email");
+        navigate("/");
+      }
     }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const updatedFormData = { ...formData, [name]: value };
+    setFormData(updatedFormData);
+    const newErrors = validateForm(updatedFormData);
+    setErrors(newErrors);
   };
 
   return (
@@ -46,11 +114,7 @@ export default function SignUp() {
       <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
         <h1 className="text-2xl font-bold mb-6 text-center">Create Account</h1>
 
-        <form
-          ref={inputForm}
-          onSubmit={handleLogin} // Call handleLogin on form submission
-          className="space-y-4"
-        >
+        <form onSubmit={handleLogin} className="space-y-4">
           <div>
             <label
               htmlFor="fullname"
@@ -64,8 +128,13 @@ export default function SignUp() {
               name="fullname"
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
               placeholder="Your preferred name"
-              required
+              onChange={handleChange}
             />
+            {errors.fullname && (
+              <span className="text-sm italic text-red-800">
+                {errors.fullname[0]}
+              </span>
+            )}
           </div>
 
           <div>
@@ -81,8 +150,13 @@ export default function SignUp() {
               name="email"
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
               placeholder="you@example.com"
-              required
+              onChange={handleChange}
             />
+            {errors.email && (
+              <span className="text-sm italic text-red-800">
+                {errors.email[0]}
+              </span>
+            )}
           </div>
 
           <div>
@@ -98,8 +172,13 @@ export default function SignUp() {
               name="password"
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
               placeholder="••••••••"
-              required
+              onChange={handleChange}
             />
+            {errors.password && (
+              <span className="text-sm italic text-red-800">
+                {errors.password[0]}
+              </span>
+            )}
           </div>
 
           <button
